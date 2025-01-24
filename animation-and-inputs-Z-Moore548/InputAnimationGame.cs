59a582fa647f5dd6ea25d091b5d04d827974d503
Z-Moore548 <zmoore1@nait.ca>
2025-01-24 10:14:39 -0700

    Completed Movement and different sprite while moving

--- animation-and-inputs-Z-Moore548/InputAnimationGame.cs
+++ animation-and-inputs-Z-Moore548/InputAnimationGame.cs
@@ -13,9 +13,12 @@ public class InputAnimationGame : Game
     private const int _WindowHeight = 380;
 
     private Texture2D _background, _cottage;
-    private CelAnimationSequence _littleGuy, _sun;
+    private CelAnimationSequence _littleGuy, _sun, _guySpin;
 
-    private CelAnimationPlayer _animation01, _animation02;
+    private CelAnimationPlayer _animationSun, _animationGuy, _animationGuySpin;
+    private KeyboardState _kbPreviousState;
+    private float _x = 60, _y = 210;
+    private bool _isMoving;
 
     public InputAnimationGame()
     {
@@ -38,21 +41,56 @@ public class InputAnimationGame : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _background = Content.Load<Texture2D>("MountainBackground");
         _cottage = Content.Load<Texture2D>("Cottage");
+        
         Texture2D spriteSheet01 = Content.Load<Texture2D>("Sun SpriteSheet");
         _sun = new CelAnimationSequence(spriteSheet01, 112, 1 / 5f);
-        _animation01 = new CelAnimationPlayer();
-        _animation01.Play(_sun);
-        Texture2D spriteSheet02 = Content.Load<Texture2D>("LittleGuySpriteSheet");
-        _littleGuy = new CelAnimationSequence(spriteSheet02, 112, 1 / 5f);
-        _animation02 = new CelAnimationPlayer();
-        _animation02.Play(_littleGuy);
+        _animationSun = new CelAnimationPlayer();
+        _animationSun.Play(_sun);
+
+        Texture2D spriteSheetMoving = Content.Load<Texture2D>("GuyMovingSprite");
+        _littleGuy = new CelAnimationSequence(spriteSheetMoving, 112, 1 / 5f);
+        _animationGuy = new CelAnimationPlayer();
+        _animationGuy.Play(_littleGuy);
+
+        Texture2D spriteSheet03 = Content.Load<Texture2D>("LittleGuyIdleSpriteSheet");
+        _guySpin = new CelAnimationSequence(spriteSheet03, 112, 1 / 4f);
+        _animationGuySpin = new CelAnimationPlayer();
+        _animationGuySpin.Play(_guySpin);
         
     }
 
     protected override void Update(GameTime gameTime)
     {
-        _animation01.Update(gameTime);
-        _animation02.Update(gameTime);
+        _animationSun.Update(gameTime);
+        _animationGuy.Update(gameTime);
+        _animationGuySpin.Update(gameTime);
+        KeyboardState kbCurrentState = Keyboard.GetState();
+        if (kbCurrentState.IsKeyDown(Keys.Down))
+        {
+            _y++;
+            _isMoving = true;
+        }
+        if (kbCurrentState.IsKeyDown(Keys.Up))
+        {
+            _y--;
+            _isMoving = true;
+        }
+        if (kbCurrentState.IsKeyDown(Keys.Left))
+        {
+            _x--;
+            _isMoving = true;
+        }
+        if (kbCurrentState.IsKeyDown(Keys.Right))
+        {
+            _x++;
+            _isMoving  = true;
+        }
+        if (kbCurrentState.IsKeyUp(Keys.Down) && kbCurrentState.IsKeyUp(Keys.Up) && kbCurrentState.IsKeyUp(Keys.Left) && kbCurrentState.IsKeyUp(Keys.Right))
+        {
+            _isMoving = false;
+        }
+        
+        _kbPreviousState = kbCurrentState;
 
         base.Update(gameTime);
     }
@@ -64,8 +102,15 @@ public class InputAnimationGame : Game
         _spriteBatch.Begin();
         _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
         _spriteBatch.Draw(_cottage, new Vector2(300f, 50f), Color.White);
-        _animation01.Draw(_spriteBatch, new Vector2(250f, 0), SpriteEffects.None);
-        _animation02.Draw(_spriteBatch, new Vector2(60f, 210f), SpriteEffects.None);
+        _animationSun.Draw(_spriteBatch, new Vector2(250f, 0), SpriteEffects.None);
+       if (!_isMoving)
+       {
+            _animationGuySpin.Draw(_spriteBatch, new Vector2(_x, _y), SpriteEffects.None);
+       }
+       else 
+       {
+            _animationGuy.Draw(_spriteBatch, new Vector2(_x, _y), SpriteEffects.None);
+       }
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- animation-and-inputs-Z-Moore548/obj/Debug/net8.0/assignment01_animation_and_input.AssemblyInfo.cs
+++ animation-and-inputs-Z-Moore548/obj/Debug/net8.0/assignment01_animation_and_input.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment01_animation_and_input")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94df8338f1e06da8c27170a618675070eae6fee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2779af5a7f44105db2dcda541c9fcf235b352a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment01_animation_and_input")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment01_animation_and_input")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
